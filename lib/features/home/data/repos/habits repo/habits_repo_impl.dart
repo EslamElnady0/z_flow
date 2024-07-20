a50be379bce07603ac906f639ec4225db851b2da
EslamElnady0 <eslamelnady254@gmail.com>
2024-07-20 07:04:06 +0300

    added data sources to constructors

--- lib/features/home/data/repos/habits repo/habits_repo_impl.dart
+++ lib/features/home/data/repos/habits repo/habits_repo_impl.dart
@@ -1,10 +1,16 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../../core/errors/failure.dart';
+import '../../data sources/habits/habits_local_data_source.dart';
+import '../../data sources/habits/habits_remote_data_source.dart';
 import '../../models/habit model/habit_model.dart';
 import 'habits_repo.dart';
 
 class HabitsRepoImpl implements HabitsRepo {
+  final HabitsLocalDataSourceImpl localDataSource;
+  final HabitsRemoteDataSourceImpl remoteDataSource;
+  HabitsRepoImpl(this.localDataSource, this.remoteDataSource);
+
   @override
   Either<Failure, Future<void>> addHabit(HabitModel habit) {
     // TODO: implement addHabit
--- lib/features/home/data/repos/tasks repo/tasks_repo_impl.dart
+++ lib/features/home/data/repos/tasks repo/tasks_repo_impl.dart
@@ -1,12 +1,18 @@
 import 'package:dartz/dartz.dart';
 
 import 'package:z_flow/core/errors/failure.dart';
+import 'package:z_flow/features/home/data/data%20sources/tasks/tasks_local_data_source.dart';
 
 import 'package:z_flow/features/home/data/models/task%20model/task_model.dart';
 
+import '../../data sources/tasks/tasks_remote_data_source.dart';
 import 'tasks_repo.dart';
 
 class TasksRepoImpl implements TasksRepo {
+  final TasksLocalDataSource tasksLocalDataSource;
+  final TasksRemoteDataSource tasksRemoteDataSource;
+  TasksRepoImpl(this.tasksLocalDataSource, this.tasksRemoteDataSource);
+
   @override
   Either<Failure, Future<void>> addTask(TaskModel task) {
     // TODO: implement addTask
