a163e2d1bde9bbf16157bcfb9f3ef2720e84adc9
EslamElnady0 <eslamelnady254@gmail.com>
2024-07-21 12:10:12 +0300

    failure class impl

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,4 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 abstract class Failure {
-  final String message;
-  Failure({required this.message});
+  final String errMessage;
+  Failure({required this.errMessage});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errMessage});
+
+  factory ServerFailure.fromFirebaseException(
+      {required FirebaseException exception}) {
+    return ServerFailure(
+      errMessage: exception.message ?? 'Server Error, please try again',
+    );
+  }
+}
+
+class NetworkFailure extends Failure {
+  NetworkFailure({required super.errMessage});
+}
+
+class CacheFailure extends Failure {
+  CacheFailure({required super.errMessage});
+
+  factory CacheFailure.fromHiveError({required HiveError error}) {
+    return CacheFailure(
+      errMessage: error.message,
+    );
+  }
 }
