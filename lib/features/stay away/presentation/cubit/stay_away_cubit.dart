94d7b3b8a26c88953de0beb5e8a5d9ced741d436
EslamElnady0 <eslamelnady254@gmail.com>
2024-07-31 07:15:55 +0300

    ..

--- lib/features/stay away/presentation/cubit/stay_away_cubit.dart
+++ lib/features/stay away/presentation/cubit/stay_away_cubit.dart
@@ -4,8 +4,6 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
 
-import '../../../../main.dart';
-
 part 'stay_away_state.dart';
 
 class StayAwayCubit extends Cubit<StayAwayState> {
@@ -14,10 +12,10 @@ class StayAwayCubit extends Cubit<StayAwayState> {
   int hours = 0;
   int mins = 0;
   Future<void> startLockService() async {
-    int durationInMillis = (hours * 60 * 60 * 1000) + (mins * 60 * 1000);
+    // int durationInMillis = (hours * 60 * 60 * 1000) + (mins * 60 * 1000);
     try {
-      await ZFlowApp.platform
-          .invokeMethod('startLockService', {'duration': durationInMillis});
+      // await ZFlowApp.platform
+      //     .invokeMethod('startLockService', {'duration': durationInMillis});
     } on PlatformException catch (e) {
       log("Failed to start lock service: '${e.message}'.");
     }
